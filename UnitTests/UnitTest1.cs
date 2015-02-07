5a54b6ccf4cced9caeaa481b3081982c984b37a4
Alexander Guan <alexander.guan.sp3@outlook.com>
2015-02-07 12:15:47 +0800

    fix the issue in unit test CanParseFileToDelivery
    close #1

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -14,6 +14,9 @@ namespace UnitTests
         {
             var filename = @"C:\Users\strategydev\Documents\GitHub\InsertDeliverySupplierFileParser\File Preparation\IC0155_31_20150112111937_10160606.upd";
             var target = filename.Load().ParseDelivery();
+            //bug fixed by ag-sp3
+            //cause analysis: Missed to load the file content from the filename
+            //solution: load the file content before parsing the raw text to delivery 
 
             var actual = target.PurchaseOrderLineNumber;
             var expected = @"71238956/132";
