7ad413445fbe165c42c742af64ae2d25514f1f9e
strategy-dev <strategy.dev@outlook.com>
2015-02-07 04:05:04 +0000

    add unit tests
    CanParseFileToDelivery

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HPDDHelper.Business.Entities.Concrete;
+using HPDDHelper.Common;
+using HPDDHelper.Data;
 
 namespace UnitTests
 {
@@ -7,8 +10,15 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanParseFileToDelivery()
         {
+            var filename = @"C:\Users\strategydev\Documents\GitHub\InsertDeliverySupplierFileParser\File Preparation\IC0155_31_20150112111937_10160606.upd";
+            var target = filename.Load().ParseDelivery();
+
+            var actual = target.PurchaseOrderLineNumber;
+            var expected = @"71238956/132";
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
